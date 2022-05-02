7317d7c6b78e870dc9831b4677c407de121c9213
VIEIRA-LIMA <paulo.vlima1971@gmail.com>
2022-05-02 17:15:31 -0300

    first commit

---
+++ Script.js
@@ -0,0 +1,32 @@
+const yourShip = document.querySelector('.player-shooter');
+const playArea = document.querySelector('#main-play-area');
+const aliensImg = ['img/monster-1.png', 'img/monster-2.png', 'img/monster-3.png'];
+const instructionsText = document.querySelector('.game-instructions');
+const startButton = document.querySelector('.start-button');
+let alienInterval;
+
+//movimento e tiro da nave
+function flyShip(event) {
+    if(event.key === 'ArrowUp') {
+        event.preventDefault();
+        moveUp();
+    } else if(event.key === 'ArrowDown') {
+        event.preventDefault();
+        moveDown();
+    } else if(event.key === " ") {
+        event.preventDefault();
+        fireLaser();
+    }
+}
+
+//função de subir
+function moveUp() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if(topPosition === "0px") {
+        return
+    } else {
+        let position = parseInt(topPosition);
+        position -= 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
